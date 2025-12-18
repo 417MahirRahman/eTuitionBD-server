c3742ed4c9e6f9258e879fb4e28495d5d2317b83
417MahirRahman <35mahirrahman@gmail.com>
2025-12-18 22:26:56 +0600

    added client-url

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const verifyJWTToken = (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     //dataBase, Collection
     const dataBase = client.db("eTuitionBD");
@@ -721,7 +721,7 @@ async function run() {
     //-----Tutor Functionalities End-----//
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
